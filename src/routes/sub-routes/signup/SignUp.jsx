5ee089ac392283991cf08ccd87bc0741f0d2ac93
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-12-14 23:50:04 +0500

    Changed Signup form

--- src/routes/sub-routes/signup/SignUp.jsx
+++ src/routes/sub-routes/signup/SignUp.jsx
@@ -1,78 +1,17 @@
-// import "./SignUp.scss"
-
-
-import { useState } from "react";
+import "./SignUp.scss"
 
 const SignUp = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: '',
-        number: '',
-      });
-    
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
-      };
+
+      
   return (
     <div>
-          <form onSubmit={handleSubmit} style={{ maxWidth: '300px', margin: '0 auto' }}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Enter username"
-          value={formData.username}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Enter password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="number">Number:</label>
-        <input
-          type="number"
-          id="number"
-          name="number"
-          placeholder="Enter number"
-          value={formData.number}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
+    <form  className="form-container">
+      <h1>SIGN UP</h1>
+        <input autoComplete="off" type="text" id="username" name="username" placeholder="Enter username" className="form-control"/>
+        <input autoComplete="off" type="email" id="email" name="email" placeholder="Enter email" className="form-control"/>
+        <input autoComplete="off" type="password" id="password" name="password" placeholder="Enter password" className="form-control"/>
+        <input autoComplete="off" type="number" id="number" name="number" placeholder="Enter number" className="form-control"/>
+        <button type="submit" className="submit-btn">Submit</button>
     </form>
     </div>
   )
